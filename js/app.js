b0eacd65e5d0840e23b627cd6109752468621d46
Feng Yunping <fengyunping1992@gmail.com>
2016-02-27 22:39:09 +0800

    Generate basic shapes & Spirographs

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
         var Puzzle = {};
         var canvas = document.getElementById('canvas');
         var ctx = canvas.getContext('2d');
-        var colorSchemes = [['#C7D8C6','#EFD9C1']];
+        var colorSchemes = [['#C7D8C6','#EFD9C1','#A9B7C0'], ['#494E6B','#985E6D','#99878F']];
         var borderWidth = 2;
         var windowWidth = window.innerWidth;
         var windowHeight = window.innerHeight;
@@ -13,6 +13,7 @@
         var indexCache = [];
         var slices = 3;
         var len = windowWidth/slices;
+        var colorIndex = 1;//generateRandom(0, colorSchemes.length-1);
         var infoArray = {};
 
         function swap(array, i, j){
@@ -35,7 +36,7 @@
         }
 
         Puzzle.init = function() {
-            document.querySelector("body").style.backgroundColor=colorSchemes[0][0];
+            document.querySelector("body").style.backgroundColor=colorSchemes[colorIndex][0];
             ctx.canvas.width = windowWidth;
             ctx.canvas.height = windowWidth;
             var mask = document.getElementById("mask");
@@ -97,12 +98,97 @@
             mask.addEventListener('click', click);
         }
         Puzzle.draw = function() {
-            var level1 = [];
-            ctx.fillStyle = colorSchemes[0][1];
-            ctx.fillRect (windowWidth/6, windowWidth/6, windowWidth/1.5, windowWidth/1.5);
+            var basicShapes = [];
+            var basicLines = [];
+            var complexLines = [];
+            var drawSquare = function(ctx){
+                ctx.fillStyle = colorSchemes[colorIndex][1];
+                ctx.fillRect (windowWidth/6, windowWidth/6, windowWidth/1.5, windowWidth/1.5);
+            };
+            var drawCircle = function(ctx) {
+                ctx.fillStyle = colorSchemes[colorIndex][1];
+                ctx.beginPath();
+                ctx.arc(windowWidth/2, windowWidth/2, windowWidth/3, 0, Math.PI*2, true); 
+                ctx.closePath();
+                ctx.fill();
+            }
+            var drawHeart = function(ctx){
+                var w = windowWidth;
+                ctx.fillStyle = colorSchemes[colorIndex][1];
+                ctx.beginPath();
+                ctx.moveTo(w/2,4*w/15);
+                ctx.bezierCurveTo(w/2,37*w/150,7*w/15,w/6,w/3,w/6);
+                ctx.bezierCurveTo(2*w/15,w/6,2*w/15,5*w/12,2*w/15,5*w/12);
+                ctx.bezierCurveTo(2*w/15,8*w/15,4*w/15,17*w/25,w/2,4*w/5);
+                ctx.bezierCurveTo(11*w/15,17*w/25,13*w/15,8*w/15,13*w/15,5*w/12);
+                ctx.bezierCurveTo(13*w/15,5*w/12,13*w/15,w/6,2*w/3,w/6);
+                ctx.bezierCurveTo(17*w/30,w/6,w/2,37*w/150,w/2,4*w/15);
+                ctx.fill();
+            }
+            function drawHypocycloids(ctx, x, y, R, r, a) {
+                var w = windowWidth;
+                ctx.save();
+                ctx.translate(x, y);
+                ctx.strokeStyle = colorSchemes[colorIndex][1];
+                
+                ctx.beginPath();
+                ctx.moveTo(R-r+a,0);
+                for (var i = 0; i < 20000; ++i) {
+                    var t = (Math.PI / 180) * i;
+                    var x = (R-r)*Math.cos(r*t/R) + a*Math.cos((1-r/R)*t);
+                    var y = (R-r)*Math.sin(r*t/R) - a*Math.sin((1-r/R)*t);
+                    ctx.lineTo(x,y);
+                }
+                ctx.stroke();
+                ctx.closePath();
+
+                ctx.restore();
+            }
+            function drawEpicycloids(ctx) {
+                var w = windowWidth;
+                ctx.save();
+                ctx.translate(w/2, w/2);
+                var R = 200;
+                var r = -70;
+                var a = 20;
+                ctx.strokeStyle = colorSchemes[colorIndex][1];
+                ctx.beginPath();
+                ctx.moveTo(R,0);
+                for (var i = 0; i < 20000; ++i) {
+                    var t = (Math.PI / 180) * i;
+                    var x = (R+r)*Math.cos(r*t/R) - a*Math.cos((1+r/R)*t);
+                    var y = (R+r)*Math.sin(r*t/R) - a*Math.sin((1+r/R)*t);
+                    ctx.lineTo(x,y);
+
+                }
+                ctx.stroke();
+                ctx.closePath();
+
+                ctx.restore();
+            }
+            var drawSpiro = function(ctx) {
+                var w = windowWidth;
+                var pos = [{'x':w/2, 'y':w/2}, {'x':w/3, 'y':w/3}];
+                
+                for (var i = 0; i < pos.length; ++i) {
+                    var x = pos[i].x;
+                    var y = pos[i].y;
+                    var R = x;
+                    var r = generateRandom(40, (R * 0.75));
+                    var a = generateRandom(35, r);
+                    drawHypocycloids(ctx, x, y, R, r, a);
+                }
+                
+                //drawEpicycloids(ctx);
+            }
+            /*basicShapes.push(drawSquare);
+            basicShapes.push(drawCircle);
+            basicShapes.push(drawHeart);
+            basicShapes[2](ctx);*/
+            drawSpiro(ctx);
         }
         Puzzle.drawGrid = function() {
-            ctx.strokeStyle = "#ffffff";
+            ctx.strokeStyle = colorSchemes[colorIndex][2];
             ctx.lineWidth=2*borderWidth;
             ctx.strokeRect(0, 0, windowWidth, windowWidth);
             
@@ -132,7 +218,7 @@
                 }
             }
 
-            for (var k = 0; k < 30; ++k) {
+            for (var k = 0; k < 0; ++k) {
                 var i = generateRandom(0, slices*slices-1);
                 var j = generateRandom(0, slices*slices-1);
                 var x1 = Math.floor(i/slices), y1 = i - x1*slices;
